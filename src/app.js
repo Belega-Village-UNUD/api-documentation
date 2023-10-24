0997b1de9d3dcb3fbc1354580c32878b7bd9392d
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-10-24 23:54:13 +0800

    EDP-40 feat: api doc with express

---
+++ src/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const fs = require("fs");
+const YAML = require("yaml");
+const swaggerUi = require("swagger-ui-express");
+const file = fs.readFileSync("./src/docs/swagger.yml", "utf8");
+const apiDocs = YAML.parse(file);
+const express = require("express");
+const bodyParser = require("body-parser");
+const { response } = require("./utils/response.js");
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", swaggerUi.serve);
+app.get("/", swaggerUi.setup(apiDocs));
+
+app.listen(process.env.SERVICE_PORT, () => {
+  try {
+    console.log("server is running");
+  } catch (err) {
+    console.log(err);
+  }
+});
